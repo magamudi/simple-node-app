630f414355491f5e6b92626b50712ee9b25c48c5
magamudi <118616073+magamudi@users.noreply.github.com>
2023-11-19 14:26:06 -0600

    modified index.json file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello, My name is Magnus Amudi Esq. An attorney and a DevOps Engineer');
+  response.send('I am an expert business and corporate attorney with over 12 years of practical experience in corporate law practice. I am an experienced document and contract drafter, negotiator and reviewer. I am Licensed in the State of New York.');
 });
 
 app.listen(8080, () => {
